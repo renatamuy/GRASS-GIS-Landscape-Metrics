0957fc06cc7311e615c1cf385acbecc35414afad
Renata Muylaert <renatamuy@gmail.com>
2017-10-30 13:44:05 -0200

    Text upgrade and format = CSV

--- scripts/test_gen_zonal_stats.py
+++ scripts/test_gen_zonal_stats.py
@@ -86,13 +86,13 @@ input_rast = ['BR_2001_euca_9']
 teststats = generalized_zonal_stats(input_shape = input_shp, input_rasters = input_rast, folder = input_dir)
 
 # Create new cols
-cols = ['prp_euca_2001'] # Column name
+cols = ['p_euc_2001'] # Column name
 col_type = ['float'] # Column type
 
-# WARNING! GRASS GIS does not like col names longer than 8-10 characters, so try to be very concise!!
+# WARNING! GRASS GIS does not like col names longer than 10 characters, so be very concise!!
 # Or later you can do something like 
 # grass.run_command('v.db.renamecolumn', map=shape_name, column='oldcolname,newcolname')
-#grass.run_command('v.db.renamecolumn', map=shape_name, column='prp_euca_2001,p_eu_2001')
+#grass.run_command('v.db.renamecolumn', map=shape_name, column='p_euc_2002,p_eu_2001')
 
 # Create cols
 teststats.create_new_column(column_names = cols, type_col=col_type)
@@ -105,27 +105,27 @@ os.chdir(input_dir)
 # export shape file
 #grass.run_command('v.out.ogr', input = shape_name, output = shape_name+'_prop_euca.shp', overwrite = True)
 # export db in csv format
-#grass.run_command('db.out.ogr', input = shape_name, output = shape_name+'_prop_euca.csv')
+#grass.run_command('db.out.ogr', input = shape_name, output = shape_name+'_prop_euca.csv', format = CSV)
 
 #------------------
 # 4.2.
-# Running for proportion of eucaliptus for only 3 years - 2001-2004
+# Running for proportion of eucaliptus for only 4 years - 2001-2004
 
 # Input shape and rasters
 input_shp = 'mun_teste_wgs84'
-input_rast = ['BR_2002_euca_9', 'BR_2003_euca_9', 'BR_2004_euca_9']
+input_rast = ['BR_2001_euca_9', 'BR_2002_euca_9', 'BR_2003_euca_9', 'BR_2004_euca_9']
 
 # Initialize and select maps to be used in zonal stats
 test_prop_euca = generalized_zonal_stats(input_shape = input_shp, input_rasters = input_rast, folder = input_dir)
 
 # Create new cols
-cols = ['prp_euca_2002', 'prp_euca_2003', 'prp_euca_2004'] # Col name
-col_type = ['float', 'float', 'float'] # Col type
+cols = ['p_euc_2001', 'p_euc_2002', 'p_euc_2003', 'p_euc_2004'] # Col name
+col_type = ['float', 'float', 'float', 'float'] # Col type
 
-# WARNING! GRASS GIS does not like col names longer than 8-10 characters, so try to be very concise!!
+# WARNING! GRASS GIS does not like col names longer than 10 characters, so be very concise!!
 # Or later you can do something like 
 # grass.run_command('v.db.renamecolumn', map=shape_name, column='oldcolname,newcolname')
-#grass.run_command('v.db.renamecolumn', map=shape_name, column='prp_euca_2001,p_eu_2001')
+#grass.run_command('v.db.renamecolumn', map=shape_name, column='p_euc_2002,p_eu_2001')
 
 # Create cols
 test_prop_euca.create_new_column(column_names = cols, type_col=col_type)
@@ -140,14 +140,14 @@ test_prop_euca.run_zonal_stats(proportion_habitat)
 end = time.time()
 
 # Print total time
-print 'The zonal stats for prop of habitat for 3 years took us '+str((end - start)/60)+' minutes.'
+print 'The zonal stats for prop of habitat for 4 years took '+str((end - start)/60)+' minutes.'
 
 # Export shapefile
 os.chdir(input_dir)
 # export shape file
 #grass.run_command('v.out.ogr', input = shape_name, output = shape_name+'_prop_euca.shp', overwrite = True)
 # export db in csv format
-#grass.run_command('db.out.ogr', input = shape_name, output = shape_name+'_prop_euca.csv')
+#grass.run_command('db.out.ogr', input = shape_name, output = shape_name+'_prop_euca.csv', format = CSV)
 
 #------------------
 # 4.3.
@@ -164,7 +164,7 @@ test_np = generalized_zonal_stats(input_shape = input_shp, input_rasters = input
 cols = ['np_2001', 'np_2002', 'np_2003', 'np_2004'] # Col names
 col_type = ['int', 'int', 'int', 'int'] # Col type
 
-# WARNING! GRASS GIS does not like col names longer than 8-10 characters, so try to be very concise!!
+# WARNING! GRASS GIS does not like col names longer than 10 characters, so be very concise!!
 
 # Create cols
 test_np.create_new_column(column_names = cols, type_col=col_type)
@@ -179,15 +179,15 @@ test_np.run_zonal_stats(number_patches, mask = True)
 end = time.time()
 
 # Print total time
-print 'The zonal stats for number of patches for 4 years took us '+str((end - start)/60)+' minutes.'
+print 'The zonal stats for number of patches for 4 years took '+str((end - start)/60)+' minutes.'
 
 # Export shapefile
 os.chdir(input_dir)
-# export shape file
+# Export shapefile. If you already run proportion of Eucalyptus, the shape_name will have both metrics calculated: zonal number of patches and zonal proportion  
 grass.run_command('v.out.ogr', input = shape_name, output = shape_name+'_prop_euca_np.shp', overwrite = True)
 
 #-------------------------------------------------------------------------
-# Do not run below!!!!
+# Do not run below!!!!!!!!!!!!!!!!!!!!!! NOT RUN!
 # Other tests
 python 
 
